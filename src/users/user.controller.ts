80427320d11e881161628738e3e72311555cd2d3
Evanio Tech <evanioshark@gmail.com>
2026-06-03 14:53:31 -0300

    usando o metodo post

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,28 +1,43 @@
-import { Controller, Get, Param, Query } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { randomUUID } from "crypto";
 
 type ParamsUser = {
     id: string;
     idEmpresa: string;
 }
 
-@Controller()
+type BodyCreateUser = {
+    name: string;
+    age: number;
+}
+
+@Controller('/users')
 export class UserController {
 
 
-    @Get('/users')
+    @Get()
     getUsers(): string {
         return "List of users";
     }
     // Example: /users/123/456
 
-    @Get('/users/:id/:idEmpresa')
+    @Get('/:id/:idEmpresa')
     findById(@Param('id') id: ParamsUser['id'], @Param('idEmpresa') idEmpresa: ParamsUser['idEmpresa']): string {
         return `User details for ID: ${id}, Company ID: ${idEmpresa}`;
     }
 
     // Example: /users/findByPages?page=1&limit=10
-    @Get('/users/findByPages')
+    @Get('/findByPages')
     findByPages(@Query('page') page: number, @Query('limit') limit: number): string {
         return `Users found by pages - Page: ${page}, Limit: ${limit}`;
+    }
+
+    @Post('/create')
+    createUser(@Body() data: BodyCreateUser) {
+        return {
+            ...data,
+            id: randomUUID(),
+            createdAt: new Date().toLocaleDateString('pt-BR'),
+        }
     }
 }
