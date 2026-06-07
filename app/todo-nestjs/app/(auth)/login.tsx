ea292bb7a1f7b0c9c2fa55525bd969572278420a
Evanio Tech <evanioshark@gmail.com>
2026-06-07 11:51:50 -0300

    feat: criado auth

---
+++ app/todo-nestjs/app/(auth)/login.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text >Login Screen</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
---
+++ app/todo-nestjs/app/(auth)/signup.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+
+const SignupScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text >Signup Screen</Text>
+    </View>
+  );
+}
+
+export default SignupScreen;
