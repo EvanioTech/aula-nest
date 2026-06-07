3ab385169dbce7e6a202df9084dcf44b1786639c
Evanio Tech <evanioshark@gmail.com>
2026-06-07 11:38:50 -0300

    feat: adicionando nativewind

---
+++ app/todo-nestjs/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+
+import "./../../global.css";
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from 'react-native';
+
+export default function TabOneScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text >Tab One</Text>
+    </View>
+  );
+}
+
+
