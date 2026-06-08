11329005549f8b06f6d592de01c42d871045bd15
Evanio Tech <evanioshark@gmail.com>
2026-06-08 09:07:26 -0300

    feat: criado componente LoginForm

--- app/todo-nestjs/app/(auth)/login.tsx
+++ app/todo-nestjs/app/(auth)/login.tsx
@@ -1,8 +1,9 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 import { cssInterop } from 'nativewind';
-import Button from '../../components/ui/button';
-import Input from '@/components/ui/input';
+import Button from '../../components/ui/Button';
+import Input from '@/components/ui/Input';
+import LoginForm from '@/components/auth/LoginForm';
 
 
 cssInterop(LinearGradient, { className: 'style' });
@@ -10,17 +11,7 @@ cssInterop(LinearGradient, { className: 'style' });
 const LoginScreen = () => {
   return (
     <LinearGradient colors={['#6B46C1', '#B794F4']} className="flex-1 items-center justify-center">
-    <View className=" justify-center items-center px-2">
-      
-        <Text className="text-4xl  text-red-300 mb-28">Todo nestjs</Text>
-        <View className="bg-white bg-opacity-20 p-6 rounded-lg w-full max-w-md items-center">
-      <Input label="Email:" placeholder="Digite seu email" />
-
-      <Input label="Senha:" placeholder="Digite sua senha" secureTextEntry={true} />
-      <Button title="Entrar" onPress={() => {}}/>
-      </View>
-      
-    </View>
+    <LoginForm title="Login" />
     </LinearGradient>
     
   );
---
+++ app/todo-nestjs/components/auth/LoginForm.tsx
@@ -0,0 +1,25 @@
+import {View, Text, Button} from 'react-native';
+import Input from '../ui/Input';
+
+interface LoginFormProps {
+  title?: string;
+}
+
+const LoginForm = ({ title }: LoginFormProps) => {
+  return (
+    <View className=" justify-center items-center px-2">
+      
+        <Text className="text-4xl  text-red-300 mb-28">{title}</Text>
+        <View className="bg-white bg-opacity-20 p-6 rounded-lg w-full max-w-md items-center">
+      <Input label="Email:" placeholder="Digite seu email" />
+
+      <Input label="Senha:" placeholder="Digite sua senha" secureTextEntry={true} />
+      <Button title="Entrar" onPress={() => {}}/>
+      </View>
+      
+    </View>
+    );
+
+};
+
+export default LoginForm;
