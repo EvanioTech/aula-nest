b0814d0392c8966560c0b469fbc3a7d8a7653045
Evanio Tech <evanioshark@gmail.com>
2026-06-09 08:04:20 -0300

    fix: ajustado api e auth usando secure store do expo para armazenar tokens

--- app/todo-nestjs/services/api.ts
+++ app/todo-nestjs/services/api.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 
 export const api = axios.create({
-  baseURL: process.env.URL,
+  baseURL: process.env.URL
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync('token');
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  
+  return config;
 });
--- app/todo-nestjs/services/auth.ts
+++ app/todo-nestjs/services/auth.ts
@@ -1,11 +1,19 @@
 import { api } from './api';
+import * as SecureStore from 'expo-secure-store';
 
 export const login = async (email: string, password: string) => {
   const response = await api.post('/auth/login', { email, password });
+  const { token } = response.data;
+  
+  await SecureStore.setItemAsync('token', token); // salva o token seguro
+  
   return response.data;
 };
 
-export const signup = async (name: string, email: string, password: string) => {
-  const response = await api.post('/auth/signup', { name, email, password });
-  return response.data;
+export const logout = async () => {
+  await SecureStore.deleteItemAsync('token');
+};
+
+export const getToken = async () => {
+  return await SecureStore.getItemAsync('token');
 };
